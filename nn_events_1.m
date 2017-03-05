453d8c1d29ec06036288b31b45653812173044dc
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2017-03-05 17:35:27 +0300

    парсим классы

--- nn_events_1.m
+++ nn_events_1.m
@@ -7,23 +7,40 @@
 
 % перва€ строка в excel-файле - заголовки, игнорируем
 
-% парсим факторы
+% парсим факторы из столбца 3
+[factors, factors_map] = parse_factors(raw, 3);
 
-% факторы в столбце 3, фразы через зап€тую
+% парсим классы из столбца 4
+[classes, classes_map] = parse_factors(raw, 4);
+
+function [factors, factors_map] = parse_factors(raw, factor_row_number)
+
+% парсим факторы/классы
+% raw - таблица из excel
+% факторы в столбце factor_row_number, фразы через зап€тую
 % на выходе в factors будут распарсенные массивы фраз
-factors = raw(:,3);
+% в factors_map будет весь список факторов по всем событи€м
 
-% здесь будет весь список факторов по всем событи€м
+factors = raw(:,factor_row_number);
 factors_map = containers.Map;
 
 for i = 2:length(factors)
     % парсим строку на фразы раздел€€ по зап€тым,
     % без пробелов в начале и конце
+    
+    single_event_factors = {};
+    
     if ischar(factors{i})
         single_event_factors = strsplit(strtrim(factors{i}),'\s*,\s*','DelimiterType','RegularExpression');
-        for curren_event_factor = single_event_factors
-            factors_map(curren_event_factor{1}) = 1;
-        end
-        factors{i} = single_event_factors;
+    elseif isfinite(factors{i})
+        single_event_factors = {num2str(factors{i})};
     end
+    
+    for curren_event_factor = single_event_factors
+        factors_map(curren_event_factor{1}) = 1;
+    end
+    
+    factors{i} = single_event_factors;
+end
+
 end
