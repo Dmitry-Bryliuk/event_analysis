c82c5eae294892b688fa199a1e4ab76631b64b2f
Dmitry-Bryliuk <dmitry.bryliuk@evote.com>
2017-03-05 14:55:16 +0300

    читаем excel из matlab, небольшой фикс

--- nn_events_1.m
+++ nn_events_1.m
@@ -1,5 +1,6 @@
 % читаем excel-файл
+% ни Excel, ни офис чтобы читать excel-файлы матлабу не нужен
 % num - матрица только числа
 % txt - матрица только текстовые значения
 % raw - всё подряд
-[num,txt,raw] = xlsread('D:\Download\event_analysis\sample_1.xls');
+[num,txt,raw] = xlsread('D:\Download\event_analysis\sample_1_1.xlsx', '', '', 'basic');
